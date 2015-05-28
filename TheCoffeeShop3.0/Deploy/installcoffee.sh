e091f4693e0800f5ff29c80b9dd34c076f33cb76
Anand Gaurav <anangaur@microsoft.com>
2015-05-28 11:46:45 +0530

    Jsons added

--- TheCoffeeShop3.0/Deploy/installcoffee.sh
+++ TheCoffeeShop3.0/Deploy/installcoffee.sh
@@ -3,5 +3,5 @@
 apt-get -y update
 apt-get -y install default-jre
 apt-get -y install tomcat7
-curl http://skynetvirus.cloudapp.net:8080/job/TheCoffeeShop.CI/28/artifact/TheCoffeeShop.war > coffee.war
+curl $1 > coffee.war
 cp coffee.war /var/lib/tomcat7/webapps/
