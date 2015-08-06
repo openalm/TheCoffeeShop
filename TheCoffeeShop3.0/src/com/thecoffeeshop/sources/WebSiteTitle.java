8299826b6b75cfea7291126d5d921477479cd307
Vijay M <pranujay@hotmail.com>
2015-08-06 09:10:49 +0530

    Removed comments

--- TheCoffeeShop3.0/src/com/thecoffeeshop/sources/WebSiteTitle.java
+++ TheCoffeeShop3.0/src/com/thecoffeeshop/sources/WebSiteTitle.java
@@ -19,21 +19,18 @@ public class WebSiteTitle extends HttpServlet {
      */
     public WebSiteTitle() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
